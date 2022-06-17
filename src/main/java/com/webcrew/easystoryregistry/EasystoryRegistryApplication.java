96bd23949ce2940cee383658fd07968ffdb0d681
Oscar <54118125+oscar-byte15@users.noreply.github.com>
2022-06-17 18:07:08 -0500

    adding eureka server

--- src/main/java/com/webcrew/easystoryregistry/EasystoryRegistryApplication.java
+++ src/main/java/com/webcrew/easystoryregistry/EasystoryRegistryApplication.java
@@ -2,8 +2,10 @@ package com.webcrew.easystoryregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EasystoryRegistryApplication {
 
 	public static void main(String[] args) {
