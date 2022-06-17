48bed7582bd3bdb3b6eb4ee64937c8d46bac837e
Edinson Cabrera Ñique <urielkillian2607@hotmail.com>
2022-06-17 17:57:23 -0500

    Agregando registry

---
+++ src/main/java/com/webcrew/easystoryregistry/EasystoryRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.webcrew.easystoryregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasystoryRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasystoryRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webcrew/easystoryregistry/EasystoryRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcrew.easystoryregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasystoryRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
